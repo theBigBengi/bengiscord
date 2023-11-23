67d2b15ccd737a11db565ec477a79b1e177ff8e9
theBigBengi <imthebigbengi@gmail.com>
2023-11-24 00:52:01 +0200

    initial profile on load

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,29 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  // Search throgh all servers and find the first one
+  // that has that profile id in one of the members of thw server
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    redirect(`/servers/${server.id}`);
+  }
+
+  // but if user is not part of any server
+  return <div>Create a server</div>;
+};
+
+export default SetupPage;
